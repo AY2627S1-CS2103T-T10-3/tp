d5fc455a2af635b5243b334c9063ccf3ab51befc
cs28118 <limcs5784@gmail.com>
2026-09-20 21:20:33 +0800

    Fix coding standard of main

--- src/main/java/seedu/address/model/person/Remark.java
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -9,6 +9,9 @@ import static java.util.Objects.requireNonNull;
 public class Remark {
     public final String value;
 
+    /**
+     * Creates a remark. An empty string represents no remark.
+     */
     public Remark(String remark) {
         requireNonNull(remark);
         value = remark;
