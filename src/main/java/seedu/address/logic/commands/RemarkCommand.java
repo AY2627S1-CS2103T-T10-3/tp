1326fac9d3e05ab97beaf0011269bd77410e65e4
Irvin <irtan757@gmail.com>
2026-09-22 23:41:21 +0800

    Fix remark command style violations
    Remove trailing whitespace and wrap long lines in remark-related production and test files.

--- src/main/java/seedu/address/logic/commands/RemarkCommand.java
+++ src/main/java/seedu/address/logic/commands/RemarkCommand.java
@@ -38,7 +38,7 @@ public class RemarkCommand extends Command {
     private final Index index;
     private final Remark remark;
 
-    /** 
+    /**
      * @param index of the eprson in the filtered person list to edit the remark
      * @param remark of the person to be updated to
      */
